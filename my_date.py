42b55b41f5b620cd5e2d91608f2a9ea13b8b2e76
ken <mbuguak@mail.gvsu.edu>
2023-09-29 09:43:58 -0400

    Hi,
    Kindly grade my project and test results.

--- my_date.py
+++ my_date.py
@@ -1,7 +1,7 @@
 #######################################################
 # my_date
 #
-# Name: zzNAMEzz (replace with your name)
+# Name: Kenneth Mbugua
 # Section: XX
 #
 # Fall 2023
@@ -9,32 +9,81 @@
 
 
 def is_leap_year(year: int) -> bool:
-    """Return True if year is a leap year, False otherwise"""
-    pass
- 
-def ordinal_date(year:int , month: int, day: int) -> int:
-    """ Return the number of days elapsed since the beginning of the year, including any partial days.
-        For example, the ordinal date for 1 January is 1.
-        Hint: pre-compute the ordinal date for the first of each month."""
-    pass
+    if (year % 4 == 0) or (year % 100 == 0) or (year % 400 == 0):
+        return True
+    else:
+        return False
+    # """Return True if year is a leap year, False otherwise"
 
-def days_elapsed(year1: int, month1: int, day1: int, year2: int, month2: int, day2:int ) -> int:
-    """ Return the number of days that have elapsed between year1-month1-day1 and year2-month2-day2.
-        You may assume that year1-month1-day1 falls on or before year2-month2-day2. (In other words,
-        your answer will always be >= 0.) """
-    pass
 
-# This is a tuple. It is immutable so that users can't accidentally modify it.
-DAYS_OF_WEEK = ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday')
+def ordinal_date(year: int, month: int, day: int) -> int:
+    day_in_month = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    if is_leap_year(year):
+        day_in_month[2] = 29
+    else:
+        day_in_month[2] = 28
+
+    sum_of_days = 0
+
+    for i, days in enumerate(day_in_month):
+        if i < month:
+            sum_of_days = sum_of_days + days
+        elif (i == 1):
+            sum_of_days = 0
+    return sum_of_days + day
+
+
+def days_elapsed(year1: int, month1: int, day1: int, year2: int, month2: int, day2: int) -> int:
+    day_in_month2 = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    if is_leap_year(year2):
+        day_in_month2[2] = 29
+    else:
+        day_in_month2[2] = 28
+    sum_of_days2 = 0
+    for i, days2 in enumerate(day_in_month2):
+        if i < month2:
+            sum_of_days2 = sum_of_days2+days2
+    sum_of_days2=sum_of_days2+day2
+    if is_leap_year(year1):
+        year1_days = 366 - ordinal_date(year1, month1, day1)
+    else:
+        year1_days = 365 - ordinal_date(year1, month1, day1)
+    return sum_of_days2+year1_days
+
+
+
+
 
 def day_of_week(year: int, month: int, day: int) -> str:
+    DAYS_OF_WEEK = ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday')
+    current_day = ("Wednesday")
+    dnum = DAYS_OF_WEEK.index(current_day)
+
+
+
+    specific_day =(days_elapsed(year,month,day,2020,5,13)-dnum)% 7
+    specific_day=7-specific_day
+    if(specific_day==7):
+        specific_day=0
+    else:
+        specific_day=specific_day
+
+    i = DAYS_OF_WEEK[specific_day]
+    return i
+
     """ Return the day of the week (Sunday, Monday, Tuesday, etc.) for the given day
         Hint 1: 1 January 1753 was a Monday.
         Hint 2: Use the methods you've already written."""
-    pass
-    
+    # pass
+
+
 def to_str(year: int, month: int, day: int) -> str:
     """ Return this date as string of the form "Wednesday, 07 March 1833"."""
-    pass
-              
-    
+    month_name = ("0","January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November",
+    "December")
+
+    return f"{day_of_week(year,month,day)}, {day}, {month_name[month]}, {year}"
+
+
+
+
--- my_date_test.py
+++ my_date_test.py
@@ -1,6 +1,7 @@
 import unittest
 import my_date
 
+
 class MyDateTest(unittest.TestCase):
 
     def test_is_leap_year1(self):
@@ -8,6 +9,35 @@ class MyDateTest(unittest.TestCase):
 
     def test_is_leap_year2(self):
         self.assertFalse(my_date.is_leap_year(1985))
+
+    def test_ordinal_date1(self):
+        self.assertEqual(my_date.ordinal_date(2020, 3, 1), 61)
+
+    def test_ordinal_date2(self):
+        self.assertEqual(my_date.ordinal_date(2023, 3, 1), 60)
+
+    def test_days_elapsed1(self):
+        self.assertEqual(my_date.days_elapsed(2020, 11, 30, 2021, 1, 2), 33)
+
+    def test_days_elapsed2(self):
+        self.assertEqual(my_date.days_elapsed(2019, 11,30,2020,3,2),93)
+    def test_day_of_week1(self):
+        self.assertEqual(my_date.day_of_week(2019,1,12), "Saturday")
+
+    def test_day_of_week2(self):
+        self.assertEqual(my_date.day_of_week(2019, 2, 20), "Wednesday")
+
+    def test_to_str1(self):
+        self.assertEqual(my_date.to_str(2019, 2, 20), "Wednesday, 20, February, 2019")
+
+    def test_to_str2(self):
+        self.assertEqual(my_date.to_str(2019,1,12),"Saturday, 12, January, 2019")
+
+    def test_to_str3(self):
+        self.assertEqual(my_date.to_str(2019,12,25),"Wednesday, 25, December, 2019")
+    def test_to_str4(self):
+        self.assertEqual(my_date.to_str(2019,12,29),"Sunday, 29, December, 2019")
+
 
 if __name__ == '__main__':
     unittest.main()
